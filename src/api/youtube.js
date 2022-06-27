cf910bbd83219458a6f721b1a8d657181f11707b
LanceParantar <20180014283@my.xu.edu.ph>
2022-06-28 01:02:09 +0800

    Configure React app for deployment to GitHub Pages

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = ""; // Access Key
+const KEY = "AIzaSyCMx8AqaWdMWYzCTkKJWD-IYy0ZOeNrpzc"; // Access Key
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
