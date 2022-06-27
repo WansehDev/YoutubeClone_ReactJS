dc9fa726ec83671def52448cf4591501fb712768
Wanseh <75514972+WansehDev@users.noreply.github.com>
2022-06-28 02:17:17 +0800

    Update youtube.js

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const KEY = "AIzaSyCMx8AqaWdMWYzCTkKJWD-IYy0ZOeNrpzc"; // Access Key
+const KEY = ""; // Access Key
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
